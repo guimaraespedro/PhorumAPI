2106dd2367e23927810ab9bd4d089643a2005cb7
guimaraespedro <pedrof.guima197@gmail.com>
2023-07-19 00:38:05 -0300

    Started to create unit tests to the project

---
+++ UnitTests/Services/UserServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Phorum.Services;
+
+namespace UnitTests.Services
+{
+    internal class UserServiceTest
+    {
+        private readonly UserService _userService;
+
+        public UserServiceTest()
+        {
+            _userService = new UserService();
+        }
+    }
+}
